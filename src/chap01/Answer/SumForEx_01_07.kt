2e499ef58b8ecd56d63389c9b87f14d14eb92a7d
zynastor <zynastorinc@gmail.com>
2022-04-05 23:39:53 +0900

    22page+answer

---
+++ src/chap01/Answer/SumForEx_01_07.kt
@@ -0,0 +1,21 @@
+package chap01.Answer
+
+import kotlin.jvm.JvmStatic
+import java.util.*
+
+// 1, 2, …, n의 합을 구합니다  (식1 + 2 + … + n = 999라고 출력함)
+internal object SumForEx_01_07 {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val stdIn = Scanner(System.`in`)
+        println("1부터 n까지의 합을 구합니다.")
+        print("n의 값：")
+        val n = stdIn.nextInt()
+        var sum = 0 // 합
+        for (i in 1..n) {
+            if (i < n) print("$i + ") else print(i)
+            sum += i // sum에 i를 더함
+        }
+        println(" = $sum")
+    }
+}
